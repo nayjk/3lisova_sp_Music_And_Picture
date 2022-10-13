754c383003f03ee448da7fab92a979b9cebbfc69
Liza <jiminpark7640@gmail.com>
2022-10-13 14:12:15 +0300

    ура победа

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.net.URL;
 import java.nio.channels.Channels;
@@ -5,17 +6,22 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
 
     public static final String IN_FILE_TXT = "inFile.txt";
-    public static final String OUT_FILE_TXT = "outFile.txt";
-    public static final String PATH_TO_MUSIC = "music";
 
+    public static final String OUT_FILEMUSIC_TXT = "outFile.txt";
+    public static final String PATH_TO_MUSIC = "music/";
+    public static final String OUT_FILEPICTURE_TXT = "outFilePicture.txt";
+    public static final String PATH_TO_PICTURE = "picture/";
     public static void main(String[] args) {
+        //MUSIC
         String Url;
         try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILEMUSIC_TXT))) {
             while ((Url = inFile.readLine()) != null) {
                 URL url = new URL(Url);
 
@@ -24,29 +30,65 @@ public class Main {
                     result = bufferedReader.lines().collect(Collectors.joining("\n"));
                 }
                 //МУЗЫКА:
-                //https://muzrecord.com/populjarnaja-muzyka/ (в файл ссылка)
+                //https://muzrecord.com (в файл ссылка)
                 //https://s.muzrecord.com/files/24kgoldn-mistakes.mp3 (для неё регулярка)
 
+                Pattern email_pattern = Pattern.compile("https:\\/\\/s.muzrecord.com\\/files\\/(.+).mp3");
+                Matcher matcher = email_pattern.matcher(result);
+                int i = 0;
+                while (matcher.find() & i < 3) {
+                    outFile.write(matcher.group() + "\r\n");
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILEMUSIC_TXT))) {
+            String music;
+            int count = 1;
+            try {
+                while ((music = musicFile.readLine()) != null) {
+                    downloadUsingNIO(music, PATH_TO_MUSIC + count + ".mp3");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //PICTURE
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFilePicture = new BufferedWriter(new FileWriter(OUT_FILEPICTURE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                String result;
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
+                }
+
+
                 //КАРТИНКИ:
-                //h//avatars.mds.yandex.net/i?id=1d7c8ca92d0f1da064e68e97cbd80680-5133450-images-thumbs&amp;n=13 (пример картинки из юрл)
-                //https://fonwall.ru/ (название сайта в файл ссылка)
-                Pattern email_pattern = Pattern.compile("h\\/\\/avatars.mds.yandex.net\\/(.+).jpg");
-               // Pattern email_pattern = Pattern.compile("https:\\/\\/s.muzrecord.com\\/files\\/(.+).mp3");
+                //https://muzrecord.com/uploads/posts/2022-10/1665098333_muzrecord_com.png
+
+                Pattern email_pattern = Pattern.compile("https:\\/\\/muzrecord.com\\/uploads\\/posts\\/(.+)muzrecord.png");
                 Matcher matcher = email_pattern.matcher(result);
                 while (matcher.find()) {
-                    outFile.write(matcher.group() + "\r\n");
+                    outFilePicture.write(matcher.group() + "\r\n");
 
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILEPICTURE_TXT))) {
             String music;
             int count = 1;
             try {
                 while ((music = musicFile.readLine()) != null) {
-                    downloadUsingNIO(music, PATH_TO_MUSIC + count + ".jpg");
+                    downloadUsingNIO(music, PATH_TO_PICTURE + count + ".png");
                     count++;
                 }
             } catch (IOException e) {
@@ -58,7 +100,6 @@ public class Main {
 
     }
 
-
     private static void downloadUsingNIO(String strUrl, String file) throws IOException {
         URL url = new URL(strUrl);
         ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
@@ -68,3 +109,6 @@ public class Main {
         byteChannel.close();
     }
 }
+
+
+
